5cd524c107dba5f58bcdcb11a00257d4ed25b6d3
Emil Pettersson <khwaaj@gmail.com>
2022-11-29 23:21:54 +0100

    Use nullptr to represent the default length expression

--- libs/gst/subtitle/TimedTextCWrapper.cpp
+++ libs/gst/subtitle/TimedTextCWrapper.cpp
@@ -4,11 +4,8 @@
 
 extern "C"
 {
-	static timedText::LengthExpression _default_len_expr(0.0, timedText::LengthUnit::percentage, timedText::Orientation::Horizontal);
-	#define DEFAULT_LEN_EXPR (CLengthExpression*)(&_default_len_expr)
-
 	CLengthExpression* default_length_expression() {
-		return DEFAULT_LEN_EXPR;
+		return nullptr;
 	}
 
 	CLengthExpression* create_length_expression(double value, CLengthUnit unit, COrientation orientation) {
@@ -22,34 +19,32 @@ extern "C"
 	}
 
 	CLengthExpression* copy_length_expression(CLengthExpression* len_expr) {
-		if (DEFAULT_LEN_EXPR == len_expr)
-			return len_expr;
 		if (!len_expr)
-			return NULL;
+			return len_expr;
 		auto* len_expr_copy = new timedText::LengthExpression(*reinterpret_cast<timedText::LengthExpression*>(len_expr));
 		return reinterpret_cast<CLengthExpression*>(len_expr_copy);
 	}
 
 	CLengthExpression* writable_length_expression(CLengthExpression* len_expr) {
-		if (DEFAULT_LEN_EXPR != len_expr)
+		if (len_expr)
 			return len_expr;
-		auto* len_expr_copy = new timedText::LengthExpression(*reinterpret_cast<timedText::LengthExpression*>(len_expr));
+		auto* len_expr_copy = new timedText::LengthExpression();
 		return reinterpret_cast<CLengthExpression*>(len_expr_copy);
 	}
 
 	void free_length_expression(CLengthExpression* len_expr) {
-		if (DEFAULT_LEN_EXPR == len_expr)
-			return;
 		delete reinterpret_cast<timedText::LengthExpression*>(len_expr);
 	}
 
 	int32_t to_pixel(CLengthExpression* len_expr, int32_t width, int32_t height) {
+		if (!len_expr)
+			return 0;
 		timedText::LengthExpression* original_len_expr = reinterpret_cast<timedText::LengthExpression*>(len_expr);
 		return original_len_expr->toPixel(timedText::Point<timedText::Px>{ width, height });
 	}
 
 	static bool is_default_length_expression(CLengthExpression* len_expr) {
-		return DEFAULT_LEN_EXPR == len_expr;
+		return !len_expr;
 	}
 
 	/////////////////////////////////////////////////////////////////////////////////
